d094b8c78324772cf136c6d144f288efed1ff062
OlegDokuchaev <58179097+OlegDokuchaev@users.noreply.github.com>
2025-05-12 18:47:15 +0000

    u

--- test.sh
+++ test.sh
@@ -3,3 +3,8 @@ launchctl print "gui/$(id -u mobile)/$BID"
 echo $?
 launchctl print "gui/$(id -u mobile)/$BID"
 echo $?
+BID="$(uicache -l | grep -i roblox | cut -d ' ' -f1)"
+launchctl print "gui/$(id -u mobile)/$BID"
+echo $?
+launchctl print "gui/$(id -u mobile)/$BID"
+echo $?
