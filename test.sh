bfdcedb5c075746417d4733b6ebb6cd0c53065b9
OlegDokuchaev <58179097+OlegDokuchaev@users.noreply.github.com>
2025-05-12 19:05:04 +0000

    u

--- test.sh
+++ test.sh
@@ -1,8 +1,11 @@
 # 1. Берём bundle-ID мода
 BID=$(uicache -l | grep -i codex | cut -d' ' -f1)
+echo "$BID"
 
 # 2. UID mobile
 UID=$(id -u mobile)
+echo "$UID"
 
 # 3. Печатаем агент
-launchctl asuser "$UID" launchctl print "gui/$UID/$BID" && echo "EXIT=$?"
+launchctl asuser "$UID" launchctl print "gui/$UID/$BID"
+echo "EXIT=$?"
