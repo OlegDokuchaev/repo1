5e23eddd1deeb0645aa998425ec1e3e7a953a395
OlegDokuchaev <58179097+OlegDokuchaev@users.noreply.github.com>
2025-05-12 17:22:18 +0000

    u

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 APP="$(uicache -l | grep -i roblox | cut -d ' ' -f1)"
 
 sqlite3 /var/mobile/Library/FrontBoard/applicationState.db " \
-SELECT key_tab.key, value \
+SELECT key_tab.key, hex(value) \
 FROM   kvs \
 JOIN   application_identifier_tab ait ON kvs.application_identifier = ait.id \
 JOIN   key_tab                         ON kvs.key = key_tab.id \
