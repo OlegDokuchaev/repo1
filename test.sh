074dc18b504f326e16df04f2e4505f01a800020d
OlegDokuchaev <58179097+OlegDokuchaev@users.noreply.github.com>
2025-05-12 18:42:33 +0000

    u

--- test.sh
+++ test.sh
@@ -1,20 +1,5 @@
-is_roblox_running() {                # $1 = Bundle ID
-  echo "$(launchctl print "gui/$(id -u mobile)/$1")"
-  launchctl print "gui/$(id -u mobile)/$1" 2>/dev/null |
-    grep -q "state = running"
-}
-
-is_roblox_in_memory() {              # 0 = не в памяти
-  echo "$(launchctl print "gui/$(id -u mobile)/$1")"
-  launchctl print "gui/$(id -u mobile)/$1" 2>/dev/null > /dev/null
-}
-
 BID="$(uicache -l | grep -i codex | cut -d ' ' -f1)"
-
-if is_roblox_running "$BID"; then
-   echo "Roblox сейчас на экране (running)"
-elif is_roblox_in_memory "$BID"; then
-   echo "Roblox заморожен в памяти (suspended)"
-else
-   echo "Roblox полностью выгружен"
-fi
+launchctl print "gui/$(id -u mobile)/$BID"
+launchctl print "gui/$(id -u mobile)/$BID"
+launchctl print "$BID"
+launchctl print "$BID"
