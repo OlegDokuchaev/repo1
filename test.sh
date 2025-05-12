36c52477d8db66dfa9b89b2ef86e1fd129297ff8
OlegDokuchaev <58179097+OlegDokuchaev@users.noreply.github.com>
2025-05-12 17:11:30 +0000

    u

--- test.sh
+++ test.sh
@@ -1,5 +1,4 @@
-sqlite3 /var/mobile/Library/FrontBoard/applicationState.db " \
-SELECT hex(value) \
-FROM   kvs \
-JOIN   application_identifier_tab  ON kvs.application_identifier = application_identifier_tab.id \
-JOIN   key_tab                     ON kvs.key = key_tab.id;"
+sqlite3 /var/mobile/Library/FrontBoard/applicationState.db \
+  "SELECT key                                   \
+     FROM key_tab                               \
+ ORDER BY key;"
