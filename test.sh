c0b6e4920f5c0be582305838e535762062c8db8c
OlegDokuchaev <58179097+OlegDokuchaev@users.noreply.github.com>
2025-05-12 18:44:53 +0000

    u

--- test.sh
+++ test.sh
@@ -1,5 +1,3 @@
 BID="$(uicache -l | grep -i codex | cut -d ' ' -f1)"
 launchctl print "gui/$(id -u mobile)/$BID"
 launchctl print "gui/$(id -u mobile)/$BID"
-launchctl print "$BID"
-launchctl print "$BID"
