dead2f28560831afbae108efab8ae35552bd4803
OlegDokuchaev <58179097+OlegDokuchaev@users.noreply.github.com>
2025-05-12 17:16:58 +0000

    u

--- test.sh
+++ test.sh
@@ -1,4 +1,8 @@
-sqlite3 /var/mobile/Library/FrontBoard/applicationState.db \
-  "SELECT key                                   \
-     FROM key_tab                               \
- ORDER BY key;"
+APP="$(uicache -l | grep -i roblox | cut -d ' ' -f1)"
+
+sqlite3 /var/mobile/Library/FrontBoard/applicationState.db " \
+SELECT key_tab.key, hex(value) \
+FROM   kvs \
+JOIN   application_identifier_tab ait ON kvs.application_identifier = ait.id \
+JOIN   key_tab                         ON kvs.key = key_tab.id \
+WHERE  ait.application_identifier = '$APP';"
