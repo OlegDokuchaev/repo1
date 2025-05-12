c8ae1f998bfcfac54a12835f093f706a364c10ff
OlegDokuchaev <58179097+OlegDokuchaev@users.noreply.github.com>
2025-05-12 17:07:35 +0000

    u

--- test.sh
+++ test.sh
@@ -1,7 +1,6 @@
-sqlite3 /var/mobile/Library/FrontBoard/applicationState.db "
-SELECT hex(value)
-FROM   kvs
-JOIN   application_identifier_tab  ON kvs.application_identifier = application_identifier_tab.id
-JOIN   key_tab                     ON kvs.key = key_tab.id
-WHERE  key_tab.key = 'process-state';
-"
+sqlite3 /var/mobile/Library/FrontBoard/applicationState.db " \
+SELECT hex(value) \
+FROM   kvs \
+JOIN   application_identifier_tab  ON kvs.application_identifier = application_identifier_tab.id \
+JOIN   key_tab                     ON kvs.key = key_tab.id \
+WHERE  key_tab.key = 'process-state';"
