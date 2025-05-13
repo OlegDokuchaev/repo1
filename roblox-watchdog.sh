f40117b20d81fdfc9cf469b15448b5c7b1df0020
OlegDokuchaev <58179097+OlegDokuchaev@users.noreply.github.com>
2025-05-13 15:38:35 +0000

    u

--- roblox-watchdog.sh
+++ roblox-watchdog.sh
@@ -5,7 +5,7 @@ is_roblox_running() {
   local BID="$1"
   LABEL_PREFIX="UIKitApplication:$BID"
 
-  read pid status <<< "$(launchctl list | awk -v lbl="$LABEL_PREFIX" '$3 ~ lbl {print $1,$2}')"
+  read pid status <<< "$(/var/jb/usr/bin/launchctl list | /var/jb/usr/bin/awk -v lbl="$LABEL_PREFIX" '$3 ~ lbl {print $1,$2}')"
 
   if [[ "$pid" =~ ^[0-9]+$ ]] && [[ "$status" == 0 ]]; then
         return 0
@@ -13,11 +13,11 @@ is_roblox_running() {
   return 1
 }
 
-BUNDLE="$(uicache -l | grep -i roblox | cut -d ' ' -f1)"
+BUNDLE="$(/var/jb/usr/bin/uicache -l | /var/jb/usr/bin/grep -i roblox | /var/jb/usr/bin/cut -d ' ' -f1)"
 PLACE=142823291
 
 if ! is_roblox_running "$BUNDLE"; then
-    uiopen "roblox://placeId=${PLACE}"
+    /var/jb/usr/bin/uiopen "roblox://placeId=${PLACE}"
     logger -t roblox-watchdog "Roblox relaunched"
 else
     logger -t roblox-watchdog "Roblox is already launched"
